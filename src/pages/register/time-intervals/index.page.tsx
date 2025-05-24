f966524f21663d0124f57b1855495447b1cb2083
adriano25klein@gmail.com <you@example.com>
2025-05-24 09:08:22 -0300

    Configurando disponibilidade - Formulário de disponibilidade

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -15,8 +15,44 @@ import {
   IntervalItem,
 } from './styles';
 import { ArrowRight } from 'phosphor-react';
+import { useFieldArray, useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { getWeekDays } from '@/src/utils/get-week-days';
+
+//NOTE: Formulário --------------------------
+const timeIntervalsSchema = z.object({});
 
 export default function TimeIntervals() {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { isSubmitting, errors },
+  } = useForm({
+    defaultValues: {
+      intervals: [
+        { weekDay: 0, enabled: false, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 1, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 2, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 3, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 4, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 5, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 6, enabled: false, startTime: '08:00', endTime: '18:00' },
+      ],
+    },
+  });
+  const weekDays = getWeekDays();
+
+  const { fields } = useFieldArray({
+    control,
+    name: 'intervals',
+  });
+
+  //Quando o usuário clicar no botão "Próximo passo", o formulário será enviado
+  async function handleFormSubmit() {}
+
+  //NOTE: Formulário --------------------------
+
   return (
     <Container>
       <Header>
@@ -28,41 +64,39 @@ export default function TimeIntervals() {
         <MultiStep size={4} currentStep={3} />
       </Header>
 
-      <IntervalBox as="form">
+      <IntervalBox as="form" onSubmit={handleSubmit(handleFormSubmit)}>
         <IntervalContainer>
-          <IntervalItem>
-            <IntervalDay>
-              <Checkbox className="bg-" />
-              <Text>Segunda-feira</Text>
-            </IntervalDay>
-            <IntervalInputs>
-              <TextInput
-                size="sm"
-                type="time"
-                step={60}
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-                crossOrigin={undefined}
-              />
-            </IntervalInputs>
-          </IntervalItem>
+          {fields.map((field, index) => {
+            return (
+              <IntervalItem key={field.id}>
+                <IntervalDay>
+                  <Checkbox className="bg-" />
+                  <Text> {weekDays[field.weekDay]} </Text>
+                </IntervalDay>
+                <IntervalInputs>
+                  <TextInput
+                    size="sm"
+                    type="time"
+                    step={60}
+                    onPointerEnterCapture={undefined}
+                    onPointerLeaveCapture={undefined}
+                    crossOrigin={undefined}
+                    {...register(`intervals.${index}.startTime`)}
+                  />
 
-          <IntervalItem>
-            <IntervalDay>
-              <Checkbox />
-              <Text>Terça-feira</Text>
-            </IntervalDay>
-            <IntervalInputs>
-              <TextInput
-                size="sm"
-                type="time"
-                step={60}
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-                crossOrigin={undefined}
-              />
-            </IntervalInputs>
-          </IntervalItem>
+                  <TextInput
+                    size="sm"
+                    type="time"
+                    step={60}
+                    onPointerEnterCapture={undefined}
+                    onPointerLeaveCapture={undefined}
+                    crossOrigin={undefined}
+                    {...register(`intervals.${index}.endTime`)}
+                  />
+                </IntervalInputs>
+              </IntervalItem>
+            );
+          })}
         </IntervalContainer>
         <Button type="submit">
           Próximo passo
