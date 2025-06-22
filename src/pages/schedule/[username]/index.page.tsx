482a49acfbac5e8241c58a8b5ff64494e909e8a8
adriano25klein@gmail.com <you@example.com>
2025-06-22 09:15:34 -0300

    Calendário e disponibilidade - Página de agendamento

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,62 @@
+import { Avatar, Heading, Text } from '@ignite-ui/react';
+import { Container, UserHeader } from './styles';
+import { prisma } from '@/src/lib/prisma';
+import { GetStaticPaths, GetStaticProps } from 'next';
+
+interface ScheduleProps {
+  user: {
+    name: string;
+    bio: string;
+    avatarUrl: string;
+  };
+}
+
+export default function Schedule({ user }: ScheduleProps) {
+  return (
+    <Container>
+      <UserHeader>
+        <Avatar src={user.avatarUrl} />
+        <Heading> {user.name} </Heading>
+        <Text>{user.bio}</Text>
+      </UserHeader>
+    </Container>
+  );
+}
+
+//NOTE: This file is used to display the user's schedule page.
+// It fetches user data from the database based on the username in the URL.
+// It uses Next.js's static generation features to pre-render the page.
+// The page will be generated on demand if the user is not found, and it will revalidate every 24 hours.
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking', // Will generate pages on demand if not found
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username);
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  });
+
+  if (!user) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      user: {
+        name: user.name,
+        bio: user.bio,
+        avatarUrl: user.avatar_url,
+      },
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
